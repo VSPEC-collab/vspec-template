831cd5ef062d3e9cb57ef33d55782e592fac78a3
Ted Johnson <ted.johnson@unlv.edu>
2023-11-03 12:21:50 -0700

    replace setup.py with pyproject.toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name='vspec-template',
-    version='1.0',
-    packages=find_packages(),
-    url='https://github.com/VSPEC-collab/vspec-template',
-    author='Ted Johnson',
-    author_email='ted.johnson@unlv.edu',
-)
